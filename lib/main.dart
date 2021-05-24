e9c2ad71d226f84fdaf158383095829dd067a544
Revanthrao Srinivasalu Naidu Ravi - <revanthraosrinivasa@airasia.com>
2021-05-24 13:14:42 +0530

    updatedchanges

--- lib/main.dart
+++ lib/main.dart
@@ -30,178 +30,9 @@ class HomePage extends StatelessWidget {
             SliverList(
               delegate: SliverChildListDelegate(
                 [
-                  Container(
-                    constraints: BoxConstraints.expand(width: 420, height: 162),
-                    decoration: BoxDecoration(color: Colors.teal[300]),
-                    padding: new EdgeInsets.all(16.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[
-                                Text(
-                                  "Next , Zohor",
-                                  style: TextStyle(
-                                      color: Colors.white, fontSize: 14),
-                                ),
-                                Text(
-                                  '1:04 PM',
-                                  style: TextStyle(
-                                      color: Colors.white, fontSize: 32),
-                                ),
-                                Text(
-                                  'Starts in 02:29:46',
-                                  style: TextStyle(
-                                      color: Colors.white, fontSize: 14),
-                                ),
-                              ],
-                            ),
-                            Image.asset('assets/images/prayer_icons.png'),
-                          ],
-                        ),
-                        Spacer(flex: 16),
-                        Row(
-                          children: [
-                            Image.asset('assets/images/calendar.png'),
-                            Text(' 9 December 2020,/23 Rhabiul Akhir 144 H',
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 14)),
-                          ],
-                        ),
-                        Spacer(flex: 4),
-                        Row(
-                          children: [
-                            Image.asset('assets/images/location.png'),
-                            Text(" Kuala Lumpur",
-                                style: TextStyle(
-                                    color: Colors.white, fontSize: 14)),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    constraints: BoxConstraints.expand(width: 375, height: 200),
-                    decoration: BoxDecoration(color: Colors.white12),
-                    padding: new EdgeInsets.all(16.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Column(
-                              children: [
-                                Image.asset('assets/images/prayer.png'),
-                                Text("Prayer",
-                                    style: TextStyle(
-                                        color: Colors.black, fontSize: 14)),
-                              ],
-                            ),
-                            Column(
-                              children: [
-                                Image.asset('assets/images/qibla.png'),
-                                Text("Qibla",
-                                    style: TextStyle(
-                                        color: Colors.black, fontSize: 14)),
-                              ],
-                            ),
-                            Column(
-                              children: [
-                                Image.asset('assets/images/sadaqah.png'),
-                                Text("sadaqah",
-                                    style: TextStyle(
-                                        color: Colors.black, fontSize: 14)),
-                              ],
-                            ),
-                            Column(
-                              children: [
-                                Image.asset('assets/images/shop.png'),
-                                Text("Shop",
-                                    style: TextStyle(
-                                        color: Colors.black, fontSize: 14)),
-                              ],
-                            ),
-                          ],
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Column(
-                              children: [
-                                Image.asset('assets/images/fidyah.png'),
-                                Text("Fidyah",
-                                    style: TextStyle(
-                                        color: Colors.black, fontSize: 14)),
-                              ],
-                            ),
-                            Column(
-                              children: [
-                                Image.asset('assets/images/zakat.png'),
-                                Text("Zakat",
-                                    style: TextStyle(
-                                        color: Colors.black, fontSize: 14)),
-                              ],
-                            ),
-                            Column(
-                              children: [
-                                Image.asset('assets/images/aqiqah.png'),
-                                Text("Aqiqah",
-                                    style: TextStyle(
-                                        color: Colors.black, fontSize: 14)),
-                              ],
-                            ),
-                            Column(
-                              children: [
-                                Image.asset('assets/images/qurban.png'),
-                                Text("Qurban",
-                                    style: TextStyle(
-                                        color: Colors.black, fontSize: 14)),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                  Container(
-                    padding: new EdgeInsets.all(16.0),
-                    child: Column(
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Image.asset('assets/images/avatar.png'),
-                            Column(
-                              children: [
-                                Text(" masjid Ar Rahah",
-                                    style: TextStyle(
-                                        color: Colors.black,
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.bold)),
-                                Text(" Yesterday at 10:30AM",
-                                    style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 12,
-                                    )),
-                              ],
-                            ),
-                          ],
-                        ),
-                        Text(
-                          "#givewithikhlas bekerjasama dengan ourfarm, Masala Wheels & Yayasan Hasanah untuk projek Food4B40 di PPR Laksamana Jalan Peel, PPR Batu Muda, PPR Kepong & Jinjang hari ini. Sebanyak 220 pek makanan asas diedarkan kepada rakan-rakan kaum India sebagai … ",
-                          style: TextStyle(color: Colors.black, fontSize: 14),
-                        ),
-                      ],
-                    ),
-                  ),
+                  SecondScreen(),
+                  ThirdClass(),
+                  FourthScreen(),
                   SizedBox(
                     height: 10,
                   ),
@@ -210,21 +41,210 @@ class HomePage extends StatelessWidget {
             ),
             SliverGrid(
                 gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3,
+                  crossAxisCount: 2,
                 ),
                 delegate: SliverChildBuilderDelegate(
-                    (BuildContext context, int index) {
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      height: 109,
-                      width: 109,
-                      child: Image.asset(images[index]),
-                    ),
-                  );
-                })),
+                  (BuildContext context, int index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        height: 109,
+                        width: 109,
+                        child: Image.asset(images[index]),
+                      ),
+                    );
+                  },
+                  childCount: images.length,
+                )),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: BoxConstraints.expand(width: 420, height: 162),
+      decoration: BoxDecoration(color: Colors.teal[300]),
+      padding: new EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    "Next , Zohor",
+                    style: TextStyle(color: Colors.white, fontSize: 14),
+                  ),
+                  Text(
+                    '1:04 PM',
+                    style: TextStyle(color: Colors.white, fontSize: 32),
+                  ),
+                  Text(
+                    'Starts in 02:29:46',
+                    style: TextStyle(color: Colors.white, fontSize: 14),
+                  ),
+                ],
+              ),
+              Image.asset('assets/images/prayer_icons.png'),
+            ],
+          ),
+          Spacer(flex: 16),
+          Row(
+            children: [
+              Image.asset('assets/images/calendar.png'),
+              Text(' 9 December 2020,/23 Rhabiul Akhir 144 H',
+                  style: TextStyle(color: Colors.white, fontSize: 14)),
+            ],
+          ),
+          Spacer(flex: 4),
+          Row(
+            children: [
+              Image.asset('assets/images/location.png'),
+              Text(" Kuala Lumpur",
+                  style: TextStyle(color: Colors.white, fontSize: 14)),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ThirdClass extends StatefulWidget {
+  @override
+  _ThirdClassState createState() => _ThirdClassState();
+}
+
+class _ThirdClassState extends State<ThirdClass> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: BoxConstraints.expand(width: 375, height: 200),
+      decoration: BoxDecoration(color: Colors.white12),
+      padding: new EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  Image.asset('assets/images/prayer.png'),
+                  Text("Prayer",
+                      style: TextStyle(color: Colors.black, fontSize: 14)),
+                ],
+              ),
+              Column(
+                children: [
+                  Image.asset('assets/images/qibla.png'),
+                  Text("Qibla",
+                      style: TextStyle(color: Colors.black, fontSize: 14)),
+                ],
+              ),
+              Column(
+                children: [
+                  Image.asset('assets/images/sadaqah.png'),
+                  Text("sadaqah",
+                      style: TextStyle(color: Colors.black, fontSize: 14)),
+                ],
+              ),
+              Column(
+                children: [
+                  Image.asset('assets/images/shop.png'),
+                  Text("Shop",
+                      style: TextStyle(color: Colors.black, fontSize: 14)),
+                ],
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  Image.asset('assets/images/fidyah.png'),
+                  Text("Fidyah",
+                      style: TextStyle(color: Colors.black, fontSize: 14)),
+                ],
+              ),
+              Column(
+                children: [
+                  Image.asset('assets/images/zakat.png'),
+                  Text("Zakat",
+                      style: TextStyle(color: Colors.black, fontSize: 14)),
+                ],
+              ),
+              Column(
+                children: [
+                  Image.asset('assets/images/aqiqah.png'),
+                  Text("Aqiqah",
+                      style: TextStyle(color: Colors.black, fontSize: 14)),
+                ],
+              ),
+              Column(
+                children: [
+                  Image.asset('assets/images/qurban.png'),
+                  Text("Qurban",
+                      style: TextStyle(color: Colors.black, fontSize: 14)),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class FourthScreen extends StatefulWidget {
+  @override
+  FourthScreenState createState() => FourthScreenState();
+}
+
+class FourthScreenState extends State<FourthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: new EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.asset('assets/images/avatar.png'),
+              Column(
+                children: [
+                  Text(" masjid Ar Rahah",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold)),
+                  Text(" Yesterday at 10:30AM",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 12,
+                      )),
+                ],
+              ),
+            ],
+          ),
+          Text(
+            "#givewithikhlas bekerjasama dengan ourfarm, Masala Wheels & Yayasan Hasanah untuk projek Food4B40 di PPR Laksamana Jalan Peel, PPR Batu Muda, PPR Kepong & Jinjang hari ini. Sebanyak 220 pek makanan asas diedarkan kepada rakan-rakan kaum India sebagai … ",
+            style: TextStyle(color: Colors.black, fontSize: 14),
+          ),
+        ],
       ),
     );
   }
